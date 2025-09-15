983a9fa1272175712beb7f60c8eb6d4c8ffa7e0d
Satyamsphere <satyamsphereglobal@gmail.com>
2025-09-15 13:42:15 +0530

    added office loc in the contactus page

--- Sphere-Global/src/components/ContactUs/ContactUs.jsx
+++ Sphere-Global/src/components/ContactUs/ContactUs.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Banner from './Banner/Banner.jsx'
 import FormPage from './FormPage/FormPage.jsx'
+import Officeloc from './Officeloc/Officeloc.jsx'
+
+
 const ContactUs = () => {
 
 
@@ -8,7 +11,8 @@ const ContactUs = () => {
     <>
     <Banner/>
     <FormPage/>
-    <div>ContactUs bro</div>
+    <Officeloc/>
+
     </>
   )
 }
